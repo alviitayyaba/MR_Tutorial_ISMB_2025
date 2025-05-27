83d481d72ee57fea3fe3197d07a81d9954fb4c75
Tayyaba Alvi <alviitayyaba82@gmail.com>
2025-05-27 14:57:16 +0200

    gitpod

--- gitpod/install_packages.R
+++ gitpod/install_packages.R
@@ -1,21 +1,17 @@
 install.packages(c(
-  "devtools", "tidyverse", "ggplot2", "ggforce", "dplyr"
+  "TwoSampleMR",
+  "MRInstruments",
+  "tidyverse",
+  "ieugwasr",
+  "gwasvcf",
+  "gwasglue2",
+  "ggplot2",
+  "ggforce",
+  "dplyr"
 ), repos = "https://cloud.r-project.org")
 
-# BiocManager needed for gwasvcf
-install.packages("BiocManager", repos = "https://cloud.r-project.org")
-
-BiocManager::install(c(
-  "gwasvcf"
-))
-
-devtools::install_github("MRCIEU/TwoSampleMR")
-devtools::install_github("MRCIEU/MRInstruments")
-devtools::install_github("MRCIEU/ieugwasr")
-devtools::install_github("MRCIEU/gwasglue2")
-
-# Install genetics.binaRies for plink binary
-devtools::install_github("explodecomputer/genetics.binaRies")
-
-# Download PLINK binary
-genetics.binaRies::get_plink_binary()
+# Install from GitHub if needed:
+if (!requireNamespace("genetics.binaRies", quietly = TRUE)) {
+  install.packages("remotes", repos = "https://cloud.r-project.org")
+  remotes::install_github("MRCIEU/genetics.binaRies")
+}
